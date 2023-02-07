4aa17596a92d288361af2d9c300baabfe1d80465
Matěj Pešl <matejpesl1@gmail.com>
2023-02-07 17:23:20 +0100

    add hungary

--- src/components/form_screens/FormPersonalInfoScreen.tsx
+++ src/components/form_screens/FormPersonalInfoScreen.tsx
@@ -16,6 +16,7 @@ export interface PersonalInfoProps {
 
 const countries = [
     ["Czech Republic", "CZ"],
+    ["Hungary", "HU"],
     ["Slovakia", "SK"],
     ["Poland", "PL"],
     ["France", "FR"],
